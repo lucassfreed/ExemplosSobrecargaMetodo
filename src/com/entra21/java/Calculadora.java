9949ac55d4887654b3f3e79512b5dd8915f15cd1
lucassfreed <lukinhasdayz@hotmail.com>
2018-10-05 11:00:40 -0300

    Finalizando exemplos de Sobrecarga de Métodos

---
+++ src/com/entra21/java/Calculadora.java
@@ -0,0 +1,15 @@
+package com.entra21.java;
+
+public class Calculadora {
+
+    public void somar(int numero1, int numero2) {
+        int soma = numero1 + numero2;
+        System.out.println("Soma: " + soma);
+    }
+
+    public void somar(double numero1, int numero2) {
+        double soma = numero1 + numero2;
+        System.out.println("Soma: " + soma);
+    }
+
+}
---
+++ src/com/entra21/java/Main.java
@@ -0,0 +1,10 @@
+package com.entra21.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Calculadora hp = new Calculadora();
+        hp.somar(1, 2);
+        hp.somar(2.30, 1.20);
+    }
+}
